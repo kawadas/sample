69f85b0b3461cc1d42523b915afd14ccb204a8e1
kawada <kawadauwa17@gmail.com>
2018-08-19 08:02:07 +0000

    Implement basic login

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -10,6 +10,7 @@ class UsersController < ApplicationController
   def create
        @user = User.new(user_params)   
     if @user.save
+      log_in @user
       # 保存の成功をここで扱う。
       flash[:success] = "早く何か作ろうぜ！"
       redirect_to @user
